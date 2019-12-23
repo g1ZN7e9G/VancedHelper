79edd1f78356a9fe8211cd68ee7c8f6fef4de074
Mattis <sgtmattis@gmail.com>
2019-12-23 22:39:19 +0100

    Added custom role name functionality and a check whether the provided hex is valid.

--- commands/colourme.js
+++ commands/colourme.js
@@ -1,29 +1,47 @@
-const Dicsord = require('discord.js');
+const { prefix } = require('../config.json');
 module.exports = {
     name: 'colourme',
     description: 'Gives the Booster a coloured role.',
-    usage: '[colour in hex]',
+    usage: '[colour in hex] <custom role name (25 characters or less)>',
     aliases: ['setcolor','colorme'],
     execute(message, args) {
-        if(!args[0]) return message.channel.send('Missing input. Please refer to the `-help colourme` page.')
+        if(!args[0]) return message.channel.send(`Missing input. Please refer to the \`${prefix}help colourme\` page.`)
+
+        const validHex = /[0-9A-Fa-f]{6}/g;
+        if(!validHex.test(args[0]) || args[0].length > 6) return message.channel.send('Woah, that\'s not a valid hex.')
+        if(args[0] == '000000') return message.channel.send('For black, use hex 111111.')
+        
+        var name = ''
+        if(args[1]) for(let i = 1; i < args.length; i++) name += args[i].replace(/\W/g, '') + ' '; else name = args[0].toUpperCase() + ' '
+        name = name.slice(0, -1)
+        if(name.toLowerCase().includes('admin') || name.toLowerCase().includes('mod') || name.toLowerCase().includes('janitor')) return message.channel.send('Not funny, didn\'t laugh. Please don\'t impersonate staff, kthxbai.')
+        
+        try {
+            if(name.length > 25) return message.channel.send('Woah, calm down. Character limit is 25.')
         if (message.member.roles.some(r => r.name === "Nitro Booster")) {
             var CC = message.guild.roles.find(r => r.name == "CC")
             if (message.member.roles.some(r => r.name == "CC")) {
-                message.member.colorRole.edit({color: args[0].toUpperCase(), name: args[0].toUpperCase()})
-                return message.channel.send("Colour changed")
+                message.member.colorRole.edit({color: args[0].toUpperCase(), name: name})
+                message.channel.send("Colour changed")
+                return message.client.channels.get('658781799580827648').send(`${message.author.tag} just changed their custom role to \`${name}\`.`)
             } else {
                 message.guild.createRole({
-                    name: args[0].toUpperCase(),
+                    name: name,
                     color: args[0].toUpperCase(),
                     position: CC.position + 1,
                 })
                 .then(role => message.member.addRole(role))
                 .catch(console.error);
                 message.member.addRole(CC).catch(console.error)
-                return message.channel.send("Colour added")
+                message.channel.send("Colour added")
+                return message.client.channels.get('658781799580827648').send(`${message.author.username} just changed their custom role to \`${name}\`.`)
             }
         } else {
             return message.channel.send("You're not a booster smh")
         } 
-    },
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
 };
