63ccee5f4d713732ce48c3f7160096d54c33fe67
Paulis Gributs <pgnmaster24@gmail.com>
2019-12-23 23:26:55 +0000

    Made the check better
    Made the check better and made the black description more descriptive

--- commands/colourme.js
+++ commands/colourme.js
@@ -9,12 +9,12 @@ module.exports = {
 
         const validHex = /[0-9A-Fa-f]{6}/g;
         if(!validHex.test(args[0]) || args[0].length > 6) return message.channel.send('Woah, that\'s not a valid hex.')
-        if(args[0] == '000000') return message.channel.send('For black, use hex 111111.')
+        if(args[0] == '000000') return message.channel.send('For black, use any hex >000000 eg. 000001.')
         
         var name = ''
         if(args[1]) for(let i = 1; i < args.length; i++) name += args[i].replace(/\W/g, '') + ' '; else name = args[0].toUpperCase() + ' '
         name = name.slice(0, -1)
-        if(name.toLowerCase().includes('admin') || name.toLowerCase().includes('mod') || name.toLowerCase().includes('janitor')) return message.channel.send('Not funny, didn\'t laugh. Please don\'t impersonate staff, kthxbai.')
+        if(message.guild.roles.some(r => r.name.toLowerCase() == name.toLocaleLowerCase()) && !message.member.roles.some(r => r.name.toLowerCase() == name.toLocaleLowerCase())) return message.channel.send('Not funny, didn\'t laugh. Please don\'t impersonate staff, kthxbai.')
         
         try {
             if(name.length > 25) return message.channel.send('Woah, calm down. Character limit is 25.')
