d986a463a8fe60a3b64ff072f5ea1102c1006d38
Mahruan <sgtmattis@gmail.com>
2020-01-02 00:38:15 +0100

    .

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -14,9 +14,10 @@ module.exports = {
         .setThumbnail('https://cdn.discordapp.com/avatars/658352336787472386/7a77a973f56971532016ebc055f9c381.png')
         .setTitle('Information about the bot')
         .addField('Version', process.env.VERSION ? process.env.VERSION : "unknown", false)
-        .addField('Commit', process.env.COMMIT ? process.env.COMMIT.slice(0, 7) : "unknown", false)
+        .addField('Commit', process.env.COMMIT ? `[${process.env.COMMIT.slice(0, 7)}](https://github.com/YTVanced/VancedHelper/commit/${process.env.COMMIT})` : "unknown", false)
         .addField('Commit message', process.env.COMMIT_MESSAGE ? process.env.COMMIT_MESSAGE : "unknown", false)
 
         return message.channel.send(output)
+        
     },
 };
