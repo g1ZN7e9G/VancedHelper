e32383cfa9206d817c7baa8e5c0ded474f2af317
Tornike Khintibidze <48173186+X1nto@users.noreply.github.com>
2020-06-20 19:29:58 +0400

    Update mint.js

--- commands/Misc/mint.js
+++ commands/Misc/mint.js
@@ -17,7 +17,7 @@ module.exports = {
             'You will have to wait 10 hours for gimp to load', 
             'Does WhatsApp give you an phone number?',
             'Hazel is a sexual predator',
-            'safe to uninstall?\n[screenshot of visual studio redistributable]<',
+            'safe to uninstall?\n[screenshot of visual studio redistributable]',
             'Can i get Creative Cloud for free without CC?',
             'it\'s opensource but it\'s an backdoor',
             'How do i delete folders?',
@@ -33,7 +33,8 @@ module.exports = {
             'google never lies',
             'i dont use any google services except android',
             'TOR is not only used for darkweb it\'s supposed to be for privacy',
-            'OgYoutube is made for privacy'
+            'OgYoutube is made for privacy',
+            'shut up'
         ];
         const output = functions.newEmbed()
             .setAuthor('Mint', 'https://cdn.discordapp.com/avatars/714767524243439738/2cdc91ae9f66ccf062a37967d6a24806.png')
