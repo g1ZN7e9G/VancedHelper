10127ceb16d77c1f8d7f6b7deb22cb2446892570
Tornike Khintibidze <48173186+X1nto@users.noreply.github.com>
2020-06-20 19:28:19 +0400

    Fixed error with command

--- commands/Misc/mint.js
+++ commands/Misc/mint.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'mint',
     description: 'Displays Quotes by famous idiot Mint',
     usage: ' ',
-    aliases: '0iq',
+    aliases: ['0iq'],
     guildonly: false,
     developersOnly: false,
     args: false,
@@ -11,24 +11,29 @@ module.exports = {
     category: 'Misc',
     execute(message) {
         const quotes = [
-            'I bought minecraft for 40 dollars',
+            'I bought it for 40 dollars',
             'Being gay isn\'t a sin',
             'I do pixel art \n And gimp is best for that',
-            'You will have to wait 10 hours for gimp to load',
-            'MiUI is the best ROM',
-            'Xiaomi has best apps',
+            'You will have to wait 10 hours for gimp to load', 
             'Does WhatsApp give you an phone number?',
             'Hazel is a sexual predator',
-            'Ii Visual Studio safe to uninstall?',
+            'safe to uninstall?\n[screenshot of visual studio redistributable]<',
             'Can i get Creative Cloud for free without CC?',
-            'I bought Minecraft Cheat for 30 dollars',
+            'it\'s opensource but it\'s an backdoor',
             'How do i delete folders?',
             'What is Google?',
             'I edited registry without backing it up',
-            'Can you export regedit and give it to me?',
+            'can you open regedit export it and give it to me pretty please\ni messed with it without backing it up',
             'What\'s official Java download link?',
             'I am cute',
-            'Make malware that uninstalls microsoft edge pls'
+            'Make malware that uninstalls microsoft edge pls',
+            'Im not saying that opensource means its safe',
+            'Opensource software tends to suck',
+            '[github link] - is this opensource? i can\'t tell',
+            'google never lies',
+            'i dont use any google services except android',
+            'TOR is not only used for darkweb it\'s supposed to be for privacy',
+            'OgYoutube is made for privacy'
         ];
         const output = functions.newEmbed()
             .setAuthor('Mint', 'https://cdn.discordapp.com/avatars/714767524243439738/2cdc91ae9f66ccf062a37967d6a24806.png')
