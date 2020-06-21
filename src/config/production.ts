53b2a1dd266039c071e07645cacd69436b4e1b0e
Mattis VenNeptury <sgtmattis@gmail.com>
2020-06-21 20:32:21 +0200

    Add config

--- src/config/production.ts
+++ src/config/production.ts
@@ -5,12 +5,12 @@ const config: Config = {
 	mongoString: process.env.MONGO_STRING!,
 	coinlibToken: process.env.COINLIB_TOKEN!,
 	defaultPrefix: '-',
-	developers: ['265560538937819137'],
+	developers: ['265560538937819137', '256143257472335872', '202115709231300617'],
 	channels: {
-		info: '',
-		errors: '',
-		boosters: '',
-		testers: ''
+		info: '658781799580827648',
+		errors: '663451700329185320',
+		boosters: '655495772405497895',
+		testers: '358967484243640321'
 	}
 };
 
