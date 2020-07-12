03983e341796c30030d46916a331969960bc83e3
X1nto <burtkni@yandex.ru>
2020-07-12 16:47:51 +0400

    codacy is being gae

--- src/commands/Fun/iq.ts
+++ src/commands/Fun/iq.ts
@@ -1,9 +1,10 @@
 import { Command, Message } from '../../Client';
 
 const callback = async (msg: Message, args: string[]) => {
-	const user = msg.mentions.users.first()?.username || (await msg.client.users.fetch(args[0]).catch(() => null))?.username || args.join(' ') || msg.author.username;
+	const user =
+		msg.mentions.users.first()?.username || (await msg.client.users.fetch(args[0]).catch(() => null))?.username || args.join(' ') || msg.author.username;
 
-	const iq = ["xinto", "x1nto"].some(n => user.toLowerCase().includes(n)) ? 5 : user.toLowerCase().includes("mint") ? 1 : Math.floor(Math.random() * 301);
+	const iq = ['xinto', 'x1nto'].some(n => user.toLowerCase().includes(n)) ? 5 : user.toLowerCase().includes('mint') ? 2 : Math.floor(Math.random() * 301);
  
 	const embed = msg.client.newEmbed('BASIC').setTitle('IQ Machine').setDescription(`${user} has an IQ of **${iq}**`);
 
